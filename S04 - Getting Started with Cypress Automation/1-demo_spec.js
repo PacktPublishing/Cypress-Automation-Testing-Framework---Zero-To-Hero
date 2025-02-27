d3c50eb4adb3708a5fb8569fbf99b34e08ffd7fe
GeethapriyaNPackt <156998994+GeethapriyaNPackt@users.noreply.github.com>
2025-02-27 11:20:41 +0530

    Old Code File Deleted

--- S04 - Getting Started with Cypress Automation/1-demo_spec.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-    it('Verify true is true!', () => {
-      expect(true).to.equal(true)
-    })
-
-    it('Verify true is false!', () => {
-        expect(true).to.equal(false)
-    })
-  })
