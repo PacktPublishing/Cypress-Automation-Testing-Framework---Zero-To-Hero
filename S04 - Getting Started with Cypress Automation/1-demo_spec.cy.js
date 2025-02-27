e396f53d628dd0af3d245ad5471ad4a827affc32
GeethapriyaNPackt <156998994+GeethapriyaNPackt@users.noreply.github.com>
2025-02-27 11:19:46 +0530

    Code file added

---
+++ S04 - Getting Started with Cypress Automation/1-demo_spec.cy.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+
+    it('Does not do much!', () => {
+        expect(true).to.equal(false)
+    })
+  })
